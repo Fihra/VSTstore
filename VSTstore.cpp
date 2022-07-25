6ea2aabe12045f7348455405585b3a3397dfbbea
fihra <fabrofabian@gmail.com>
2022-07-24 20:11:01 -1000

    add project

---
+++ VSTstore.cpp
@@ -0,0 +1,17 @@
+// VSTstore.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+void MainSelections()
+{
+    std::cout << "Instruments" << std::endl;
+    std::cout << "Signal Processing" << std::endl;
+    std::cout << "Spectrum Analysers" << std::endl;
+}
+
+int main()
+{
+    std::cout << "Welcome to the VST Shop!\n";
+    MainSelections();
+}
